1299e2a7b2c96aa25f5ba7b3ec007536c0ee5e18
egemoroglu <egemoroglu@hotmail.com>
2024-04-18 14:41:36 +0300

    Terraform main and resources

---
+++ Terraform/resources.tf
@@ -0,0 +1,80 @@
+resource "aws_dynamodb_table" "egemoroglu-users" {
+    name           = "egemoroglu-users"
+    billing_mode   = "PROVISIONED"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "userId"
+    attribute {
+        name = "userId"
+        type = "S"
+    }
+    attribute {
+        name = "username"
+        type = "S"
+    }
+    attribute {
+        name = "password"
+        type = "S"
+    }
+    global_secondary_index {
+        name = "username-index"
+        hash_key = "username"
+        projection_type = "ALL"
+        read_capacity = 5
+        write_capacity = 5
+    }
+    global_secondary_index {
+        name = "password-index"
+        hash_key = "password"
+        projection_type = "ALL"
+        read_capacity = 5
+        write_capacity = 5
+    }
+  
+}
+
+resource "aws_dynamodb_table" "egemoroglu-todos" {
+    name = "egemoroglu-todos"
+    billing_mode = "PROVISIONED"
+    read_capacity = 5
+    write_capacity = 5
+    hash_key = "todoId"
+    attribute {
+        name = "todoId"
+        type = "S"
+    }
+    attribute {
+        name = "title"
+        type = "S"
+    }
+    attribute {
+        name = "assignee"
+        type = "S"
+    }
+    attribute {
+        name = "status"
+        type = "S"
+    }
+    global_secondary_index {
+        name = "assignee-index"
+        hash_key = "assignee"
+        projection_type = "ALL"
+        read_capacity = 5
+        write_capacity = 5
+    }
+    global_secondary_index {
+        name = "status-index"
+        hash_key = "status"
+        projection_type = "ALL"
+        read_capacity = 5
+        write_capacity = 5
+    }
+    global_secondary_index {
+        name = "title-index"
+        hash_key = "title"
+        projection_type = "ALL"
+        read_capacity = 5
+        write_capacity = 5
+    }
+  
+}
